c92e7a48824cd84ec837937ec1ca25af44b6c64b
George Salamanca Jr <georgesalamancajr@Georges-Mac-mini.local>
2018-05-05 14:40:54 -0400

    Add GET/Todos route

--- server/server.js
+++ server/server.js
@@ -20,6 +20,14 @@ res.status(400).send(e.message)
 })
 });
 
+app.get("/todos", (req, res)=>{
+    Todo.find().then((todos)=>{
+        res.send({todos})
+    }, (e)=>{
+res.status(400).send(e.message)
+    })
+})
+
 app.listen(3000, ()=>{
     console.log('started on port 3000');
     
