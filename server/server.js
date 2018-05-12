4301d51f7cf0160693a301c7c841f3dbcdcb5c39
George Salamanca Jr <georgesalamancajr@Georges-Mac-mini.local>
2018-05-11 22:07:28 -0400

    add GET users/me and authentication mware

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const {ObjectID} =require("mongodb")
 var {mongoose} = require("./db/mongoose")
 var {Todo} = require("./models/todo")
 var {User} = require("./models/user")
-
+var {authenticate} = require("./middleware/authenticate")
 var app = express();
 const port = process.env.PORT;
 
@@ -109,6 +109,13 @@ app.post("/users", (req, res)=>{
         
         res.status(400).send(err.message)})
 })
+
+
+
+
+app.get("/users/me", authenticate, (req, res)=>{
+ res.send(req.user);
+});
 
 app.listen(port, ()=>{
     console.log(`started on port ${port}`);
